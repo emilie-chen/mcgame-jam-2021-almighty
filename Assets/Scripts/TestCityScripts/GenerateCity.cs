8591492f20a89b278c3113fe60ea16057d9219d1
njchensl <nanjingchj@gmail.com>
2021-01-16 17:32:32 -0500

    destroying buildings

--- Assets/Scripts/TestCityScripts/GenerateCity.cs
+++ Assets/Scripts/TestCityScripts/GenerateCity.cs
@@ -8,22 +8,6 @@ public class GenerateCity : MonoBehaviour
  
     void Start()
     {
-        GameObject buildingTemplate = GameObject.FindGameObjectWithTag("Building");
-        for (int i = 0; i < 2; i++)
-        {
-            for (int j = 0; j < 2; j++)
-            {
-                for (int x = 0; x < 7; x++)
-                {
-                    for (int y = 0; y < 7; y++)
-                    {
-                        GameObject newBuilding = Instantiate(buildingTemplate);
-                        newBuilding.transform.position = buildingTemplate.transform.position + new Vector3(i * 100 + x * 12, 0, j * 100 + y * 12);
-                    }
-                }
-            }
-        }
-        NavMeshBuilder.BuildNavMesh();
     }
 
     void Update()
